2bd47bc5ca0f04e5629b8f90f58d65364e065355
admin <23DH110578@st.huflit.edu.vn>
2025-03-09 21:17:18 +0700

    tạo fragment

--- app/src/main/java/com/example/onlineshop/MainActivity.java
+++ app/src/main/java/com/example/onlineshop/MainActivity.java
@@ -1,15 +1,42 @@
 package com.example.onlineshop;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.onlineshop.fragments.HomeFragment;
+import com.example.onlineshop.fragments.NotificationsFragment;
+import com.example.onlineshop.fragments.ProfileFragment;
+import com.example.onlineshop.fragments.WishListFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addControls();
+        handleEvents();
 
 
     }
+
+    private void handleEvents() {
+
+
+    }
+
+    private void addControls() {
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+
+
+    }
+
+
 }
