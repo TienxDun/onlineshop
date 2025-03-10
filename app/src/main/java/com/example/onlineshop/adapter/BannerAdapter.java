aae7e9d7470364a7ec916cc7b06d8ab9c72599d6
admin <23DH110578@st.huflit.edu.vn>
2025-03-11 03:38:05 +0700

    banner

---
+++ app/src/main/java/com/example/onlineshop/adapter/BannerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.onlineshop.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.onlineshop.R;
+
+import java.util.List;
+
+public class BannerAdapter extends RecyclerView.Adapter<BannerAdapter.BannerViewHolder> {
+    private Context context;
+    private List<Integer> bannerList;
+
+    public BannerAdapter(Context context, List<Integer> bannerList) {
+        this.context = context;
+        this.bannerList = bannerList;
+    }
+
+    @NonNull
+    @Override
+    public BannerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_banner, parent, false);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+        view.setLayoutParams(layoutParams);
+        return new BannerViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull BannerViewHolder holder, int position) {
+        Glide.with(context).load(bannerList.get(position)).into(holder.imgBanner);
+    }
+
+    @Override
+    public int getItemCount() {
+        return bannerList.size();
+    }
+
+    static class BannerViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgBanner;
+
+        public BannerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgBanner = itemView.findViewById(R.id.imgBanner);
+        }
+    }
+}
--- app/src/main/java/com/example/onlineshop/fragments/HomeFragment.java
+++ app/src/main/java/com/example/onlineshop/fragments/HomeFragment.java
@@ -10,8 +10,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.onlineshop.R;
+import com.example.onlineshop.adapter.BannerAdapter;
 import com.example.onlineshop.adapter.ProductAdapter;
 import com.example.onlineshop.model.Product;
 
@@ -19,21 +21,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
+    private ViewPager2 viewPagerBanner;
     private RecyclerView recyclerView;
     private ProductAdapter productAdapter;
     private List<Product> productList;
+    private List<Integer> bannerList;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        viewPagerBanner = view.findViewById(R.id.viewPagerBanner);
         recyclerView = view.findViewById(R.id.recyclerPopular);
+
+        // Cấu hình RecyclerView
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
 
+        // Load sản phẩm
         loadProducts();
         productAdapter = new ProductAdapter(getContext(), productList);
         recyclerView.setAdapter(productAdapter);
+
+        // Load banner
+        loadBanner();
+        BannerAdapter bannerAdapter = new BannerAdapter(getContext(), bannerList);
+        viewPagerBanner.setAdapter(bannerAdapter);
 
         return view;
     }
@@ -46,4 +59,10 @@ public class HomeFragment extends Fragment {
         productList.add(new Product("Máy tính MacBook Pro M3", R.drawable.pic5, 34990000, 0.05, 4.7f, "Hồ Chí Minh"));
     }
 
+    private void loadBanner() {
+        bannerList = new ArrayList<>();
+        bannerList.add(R.drawable.pic1);
+        bannerList.add(R.drawable.pic2);
+        bannerList.add(R.drawable.pic3);
+    }
 }
